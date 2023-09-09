2e26b31b8f9ee1e168201d6c3ffd35edf07e9b9f
Alexandr Lesnichenko <admin@webgoodwin.com>
2023-09-09 14:44:19 +0300

    fix iter5

---
+++ internal/server/handlers/handlers_test.go
@@ -0,0 +1,57 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestMainPage(t *testing.T) {
+	type want struct {
+		code        int
+		response    string
+		contentType string
+	}
+	tests := []struct {
+		name string
+		want want
+	}{
+		{
+			name: "positive test #1",
+			want: want{
+				code:        200,
+				response:    `{"status":"ok"}`,
+				contentType: "text/html; charset=utf-8",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodGet, "/", nil)
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+			MainPage(w, request, httprouter.Params{})
+
+			res := w.Result()
+			// проверяем код ответа
+			assert.Equal(t, res.StatusCode, test.want.code)
+			// получаем и проверяем тело запроса
+			defer func(Body io.ReadCloser) {
+				err := Body.Close()
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+			}(res.Body)
+			_, err := io.ReadAll(res.Body)
+
+			require.NoError(t, err)
+			//assert.JSONEq(t, string(resBody), test.want.response)
+			assert.Equal(t, res.Header.Get("Content-Type"), test.want.contentType)
+		})
+	}
+}
