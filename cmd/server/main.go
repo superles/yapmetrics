82801e7cca8b1cdd80bfbcee82b9b6f19773dc12
Alexandr Lesnichenko <admin@webgoodwin.com>
2023-10-20 19:09:23 +0300

    add iter11

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"github.com/superles/yapmetrics/internal/server"
 	"github.com/superles/yapmetrics/internal/server/config"
+	storage2 "github.com/superles/yapmetrics/internal/storage"
+	"github.com/superles/yapmetrics/internal/storage/memstorage"
 	"github.com/superles/yapmetrics/internal/storage/pgstorage"
 )
 
 func main() {
 	cfg := config.New()
-	storage := pgstorage.New(cfg.DatabaseDsn)
+	var storage storage2.Storage
+	storage = memstorage.New()
+	if len(cfg.DatabaseDsn) > 0 {
+		storage = pgstorage.New(cfg.DatabaseDsn)
+	}
 	srv := server.New(storage, cfg)
 	srv.Run()
 }
