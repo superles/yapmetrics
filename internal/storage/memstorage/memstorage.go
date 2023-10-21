5eac9b13ec9b757a3a052860e704a6985c7f3878
Alexandr Lesnichenko <admin@webgoodwin.com>
2023-10-21 03:02:49 +0300

    catching casper

--- internal/storage/memstorage/memstorage.go
+++ internal/storage/memstorage/memstorage.go
@@ -8,7 +8,6 @@ import (
 )
 
 var storageSync = sync.Mutex{}
-var storageAllSync = sync.Mutex{}
 
 type MemStorage struct {
 	collection map[string]types.Metric
@@ -48,8 +47,6 @@ func (s *MemStorage) Set(ctx context.Context, data *types.Metric) {
 }
 
 func (s *MemStorage) SetAll(ctx context.Context, data *[]types.Metric) error {
-	storageAllSync.Lock()
-	defer storageAllSync.Unlock()
 	for _, value := range *data {
 		logger.Log.Sugar().Debug("SetAll", value)
 		switch value.Type {
